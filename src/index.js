188d922e78e011115acba9c67b2076ee03db4952
Syam Son Penumala <syamson132000@gmail.com>
2025-06-04 21:26:52 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import UseMemo from "./components/UseMemo";      // For input validation with useMemo
-import TodoItem from "./components/ReactMemo";   // For memoized todo list items
+
+ReactDOM.render(<App />, document.getElementById("root"));
